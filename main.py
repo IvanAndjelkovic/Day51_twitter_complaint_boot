c57dfab127462849893199e3a71dc06388404f7c
IvanAndjelkovic <ivan.andelkovic707@gmail.com>
2025-12-21 20:41:29 +0100

    setting up the class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+from selenium import webdriver
+
 
 
 load_dotenv()
@@ -11,3 +13,29 @@ twitter_pass=os.getenv("TWITTER_PASSWORD")
 
 
 
+PROMISED_DOWN=150
+PROMISED_UP=10
+# CHROME_DRIVER_PATH="/usr/local/bin/chromedriver"
+twitter_mail=os.getenv("TWITTER_EMAIL")
+twitter_pass=os.getenv("TWITTER_PASSWORD")
+
+
+
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.down = 0
+        self.up = 0 
+
+    def get_internet_speed(self):
+        print("vrlo dobro")
+    def tweet_at_provider(self):
+        print("mile")
+
+
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
